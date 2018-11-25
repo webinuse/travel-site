c73203eb84aee94a5c5264a1e0e8c99bc73abbcb
Amer <amer@webinuse.com>
2018-11-25 01:32:42 +0100

    Added several CSS styles, started editing CSS and making page more beautiful and functional

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,10 @@
 var gulp = require("gulp");
 var watch = require("gulp-watch");
 var postcss = require("gulp-postcss");
+var autoprefixer = require("autoprefixer");
+var cssvars = require("postcss-simple-vars");
+var nested = require("postcss-nested");
+var cssImport = require("postcss-import");
 
 gulp.task('default', function() {
   console.log("Hoooray! you created Gulp task");
@@ -12,7 +16,7 @@ gulp.task('html', function() {
 
 gulp.task('styles', function() {
   return gulp.src('./app/assets/styles/styles.css')
-    .pipe()
+    .pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
     .pipe(gulp.dest('./app/temp/styles'));
 })
 
