b42a06449a749a09ec77038907b6d1f9cbc4e40b
Amer <amer@webinuse.com>
2018-11-25 17:27:35 +0100

    Before branches

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -4,9 +4,11 @@ var autoprefixer = require("autoprefixer");
 var cssvars = require("postcss-simple-vars");
 var nested = require("postcss-nested");
 var cssImport = require("postcss-import");
+var cssMixins = require("postcss-mixins");
+
 gulp.task('styles', function() {
   return gulp.src('./app/assets/styles/styles.css')
-    .pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
+    .pipe(postcss([cssImport,cssMixins, cssvars, nested, autoprefixer]))
     .on('error', function(errorInfo) {
       console.log(errorInfo.toString());
       this.emit('end');
